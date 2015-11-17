4518c8727d134abc1bb98974b6b1dadfc03c3877
Joe Palala <joe@mentorsdojo.com>
2015-11-17 19:40:46 +0800

    fixed json post

--- app.php
+++ app.php
@@ -1,21 +1,23 @@
 <?php
-//actual api
 require_once __DIR__ . '/vendor/autoload.php';
 $dotenv = new Dotenv\Dotenv(__DIR__);
 $dotenv->load();
 
-//if(isset($_POST['message'])) {
-   $message = '{"to": "me"}';
-   if(isJSON($message)) {
-     $msg = json_decode($message);
-     echo '---';
-     var_dump($msg->to);
-    if($_ENV['MY_SETTING']) {
-       echo $_ENV['MY_SETTING'];
-    }
-    } else {
-      var_dump($message);
-    }
-  // }.
-//}
+//sendgrid curl web api v2 based
+require('lib/mailer.php');
+$sendgrid_user = $_ENV['SENDGRID_USERNAME'];
+$sendgrid_pass = $_ENV['SENDGRID_PASSWORD'];
+if(isset($_POST['message'])) {
+  $message = $_POST['message'];
+  if(isJSON($message)) {
+    $msg = json_decode($message);
+    
+    //-> if $msg->body is html, text version of email is just the strip_tag-ed (strips out html), allowing only <br> html tag
+    sendmail($sendgrid_user,$sendgrid_pass, $msg->from, $msg->to, $msg->subject, $msg->body); 
+  }
+  //else {
+  //throw error/do something...
+  //  var_dump($message);
+  //}
+}
 
