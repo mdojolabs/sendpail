12859c6746bc4092cf3a08e0d0899f95cecf02cd
Joe Palala <joe@mentorsdojo.com>
2015-11-18 03:01:23 -0500

    fixed mailer code

--- lib/mailer.php
+++ lib/mailer.php
@@ -5,15 +5,17 @@ function sendmail($sendgrid_user,$sendgrid_pass, $from, $to, $subject, $body) {
    $url = 'https://api.sendgrid.com/';
 
   $params = array(
-      'api_user'  => $user,
-      'api_key'   => $pass,
+      'api_user'  => $sendgrid_user,
+      'api_key'   => $sendgrid_pass,
       'from'      => $from,
       'to'        => $to,
       'subject'   => $subject,
       'html'      => $body,
       'text'      => strip_tags($body),
     );
-
+  echo $to . "\r\n";
+  echo $subject . "\r\n";
+  echo $body . "\r\n";
 
   $request =  $url.'api/mail.send.json';
 
@@ -23,7 +25,7 @@ function sendmail($sendgrid_user,$sendgrid_pass, $from, $to, $subject, $body) {
   curl_setopt ($session, CURLOPT_POST, true);
 
   //auth 
-  $authorization = "Authorization: Bearer <YourBearerTokenHere SG.>";
+  $authorization = "Authorization: Bearer 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";
 
   curl_setopt($session, CURLOPT_HTTPHEADER, $authorization);
 
@@ -35,5 +37,10 @@ function sendmail($sendgrid_user,$sendgrid_pass, $from, $to, $subject, $body) {
   curl_setopt($session, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
   curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
 
+  $response = curl_exec($session);
+  curl_close($session);
+
+  // print everything out
+  print_r($response);
 
 }
